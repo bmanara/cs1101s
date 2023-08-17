86a29367675b34c8631f441ac8bf017918090a62
Source Academy User <No public email provided>
2023-08-17 14:45:48 +0800

    Source 1 code with statements/expressions

---
+++ reflection_1.js
@@ -0,0 +1,27 @@
+const x = 3; // Constant Declaration
+
+1 + 2; // Expression statement 
+
+true ? 1 + 1 : 17; // Conditional Expressions
+
+x > 0 ? x : -x; // Usage of comparison operators
+
+x === 0 ? 1 : 2; 
+
+// Nested Conditional Expression
+// true ? 1 : y < 1 ? 4711 : 42; Throws an error as y is not defined 
+
+// Function declaration
+function square(x) {
+    return x * x;
+}
+
+function hypotenuse(a, b) {
+    return math_sqrt(square(a) + square(b));
+}
+
+hypotenuse(3, 4);
+
+1 + 2 * 3 + 4;
+
+1 + 2 >= 3 ? 4 * 5 : 6 + 7;
