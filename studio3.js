c707ac66f063d183c178c4b64d12dec3951d9ff0
Source Academy User <No public email provided>
2023-08-29 16:01:01 +0800

    S3 Extra Answers

--- studio3.js
+++ studio3.js
@@ -18,8 +18,29 @@ function moony(n) {
                          stack_frac(1 / n, blank, moony(n - 1)));
 }
 
+// show(moony_1(ribbon));
+// show(moony_2(5));
+// show(moony(5));
 
+// Extra S3
+// Time: O(log(n)) Space: O(log(n))
 
-show(moony_1(ribbon));
-show(moony_2(5));
-show(moony(5));
+function fast_expt(b, n) {
+    return n <= 1
+           ? b
+           : n % 2 === 0
+           ? math_pow(fast_expt(b, n / 2), 2)
+           : b * fast_expt(b, n - 1);
+}
+
+fast_expt(2, 5);
+
+// Time: O(log(n)) Space: O(1)
+
+function iter(exp, base, product) {
+    return exp === 0
+           ? product
+           
+}
+
+// Time: O(1) Space: O(1)
