45c9f87a3deba55167d6f09579bfbd2b4b3c30cc
Source Academy User <No public email provided>
2023-09-12 18:03:04 +0800

    Changes made from Source Academy

--- masterycheck_1.js
+++ masterycheck_1.js
@@ -9,8 +9,10 @@ function factorial(n) {
            : n * factorial(n - 1);
 }
 
+factorial(5);
+
 /*
-The above function gives rise to a recursive function because it has deferred
+The above function gives rise to a recursive process because it has deferred
 operations. 
 
 Using the substitution model, we can visualise the recursive process:
@@ -24,3 +26,32 @@ factorial(5);
 5 * (4 * 6);
 5 * 24;
 120;
+
+As shown above, we have deferred operations, with every line, there is an 
+additional operation that the interpreter needs to keep track of. 
+*/
+
+// Example of iterative process
+function factorial_iter(n) {
+    function fact_iter(product, counter, max_count) {
+        return counter > max_count
+               ? product
+               : fact_iter(product * counter, counter + 1, max_count);
+    }
+    
+    return fact_iter(1, 1, n);
+}
+
+factorial_iter(5);
+
+/*
+The above function gives rise to a recursive process because it does not have 
+deferred operation.
+
+Using the substitution model, we can visualise the iterative process:
+factorial_iter(5);
+fact_iter(1, 1, 5);
+fact_iter(1 * 1, 1 + 1, 5); => fact_iter(1, 2, 5);
+fact_iter(1* 2)
+*/
+
