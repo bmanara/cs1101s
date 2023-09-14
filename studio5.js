0608567d0024f3c58135c77c0dc896f72f8f96fd
Source Academy User <No public email provided>
2023-09-14 19:01:29 +0800

    Changes made from Source Academy

--- studio5.js
+++ studio5.js
@@ -1,7 +1,9 @@
-// // Question 1
-// const LoL = list(list(1, 2), list(3, 4, 5, 6), null, list(7, 8 , 9));
+/* Start comment
+// Recitation 5
+// Question 1
+const LoL = list(list(1, 2), list(3, 4, 5, 6), null, list(7, 8 , 9));
 
-// list_to_string(LoL);
+list_to_string(LoL);
 
 // function flatten_list(xs) {
 //     function operation(x, y) {
@@ -11,24 +13,47 @@
 //             return append(flatten_list(x), y);
 //         }
 //     }
+    
 //     return accumulate(operation, null, xs);
 // }
 
-// flatten_list(LoL);
+function flatten_list(xs) {
+    return accumulate(append, null, xs);
+}
 
-// // Question 2
+flatten_list(LoL);
+
+// Question 2
 // function tree_sum(xs) {
 //     const new_list = flatten_list(xs);
 //     return accumulate((x, y) => x + y, 0, new_list);
 // }
 
-// const my_tree = list(1, list(2, list(3, 4), 5), list(6, 7));
-// tree_sum(my_tree);
+function tree_sum(tree) {
+    return is_null(tree)
+           ? 0
+           : (is_list(head(tree)) // can be replaced with is_pair for time saving
+             ? tree_sum(head(tree))
+             : head(tree))
+             + tree_sum(tail(tree));
+}
 
-// Question 3
+const my_tree = list(1, list(2, list(3, 4), 5), list(6, 7));
+tree_sum(my_tree);
+End comment
+*/
+
+
+// Question 3 
+// It's important to think about how accumulate works, it only works on a single
+// list, hence we need to decide on how to work with the head of the list, depending
+// on whether it is a list or data
+
 function accumulate_tree(f, op, initial, tree) {
     return accumulate(
-             op(f, f),
+             (x, y) => is_list(x) 
+                       ? op(accumulate_tree(f, op, initial, x), y) // wishful thinking
+                       : op(f(x), y),
              initial,
              tree);
 }
@@ -49,7 +74,7 @@ function flatten(tree) {
 const tree1 = list(1, 2, list(3, 4), list(5, list(6, 7)));
 
 const tree2 = list(1, list(list(8, 9), 10, list(11, list(12))), 
-                   null, list(3, 4), list(5, list(6, 7)));
+                  null, list(3, 4), list(5, list(6, 7)));
 
 display( tree_sum(tree1) ); // Result: 28
 display( tree_sum(tree2) ); // Result: 76
