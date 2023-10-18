39934c65cc99a2098560c6d59e873e88eb768aba
Source Academy User <No public email provided>
2023-10-18 20:41:54 +0800

    Changes made from Source Academy

--- studio9.js
+++ studio9.js
@@ -1,3 +1,4 @@
+/*
 function change(x, new_value) {
     x = new_value;
 }
@@ -83,3 +84,94 @@ L; // returns [2, [4, [6, [8, null]]]]
 // foo(1);
 // // Breakpoint #5
 // 10 + 5;
+
+*/
+
+// Reflection 9
+// Question 1
+
+function make_optimized_search(A) {
+    const len = array_length(A);
+    
+    function insertion_sort(A) {
+        
+        function find_min_pos(arr, start, end) {
+            let value = arr[start];
+            let index = start;
+            for (let j = start + 1; j < end; j = j + 1) {
+                if (value > arr[j]) {
+                    value = arr[j];
+                    index = j;
+                }
+            }
+            
+            return index;
+        }
+        
+        function swap(arr, x, y) {
+            const temp = arr[x];
+            arr[x] = arr[y];
+            arr[y] = temp;
+        }
+        
+        for (let i = 0; i < len; i = i + 1) {
+            const position = find_min_pos(A, i, len);
+            swap(A, i, position);
+        }
+    }
+    
+    function binary_search(A, x, start, end) {
+        if (start > end) {
+            return false;
+        }
+        
+        const mid = math_floor((start + end) / 2);
+        const result = A[mid] === x || (x < A[mid]
+                                        ? binary_search(A, x, start, mid - 1)
+                                        : binary_search(A, x, mid + 1, end));
+        return result;
+    }
+    
+    const sorted_array = insertion_sort(A);
+    
+    return x => binary_search(A, x, 0, len - 1);
+}
+
+// const my_array = [3,41,20,1,5,16,4,0,14,6,17,8,4,0,2];
+// const my_search = make_optimized_search(my_array);
+
+// my_search(14);
+// my_search(15);
+// my_search(30);
+
+
+// Question 2a
+function fib(n) {
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
