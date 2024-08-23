ff57c16f42aeddb99eb13887b119beed292d8d6a
Source Academy User <Brian Ma>
2024-08-23 12:04:58 +0800

    Changes made from Source Academy

--- 2425studio3.js
+++ 2425studio3.js
@@ -1,3 +1,6 @@
+// Function Application
+
+/*
 const cost_per_meter = 199.95;
 
 function circumference(radius) {
@@ -9,4 +12,25 @@ function cost_of_circular_handrail(r) {
 }
 
 cost_of_circular_handrail(2.1);
+*/
 
+// Substitution Model
+
+/*
+function square(x) {
+    return x * x;
+}
+
+function sum_of_squares(x, y) {
+    return square(x) + square(y);
+}
+
+function f(a) {
+    return sum_of_squares(a + 1, 2 * a);
+}
+
+f(5); 
+*/
+
+
+// Recursion
