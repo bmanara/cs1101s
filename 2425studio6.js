145fd319d7c9f77f848b7850926e53a58834ba1f
Source Academy User <Brian Ma>
2024-09-13 15:56:23 +0800

    Changes made from Source Academy

--- 2425studio6.js
+++ 2425studio6.js
@@ -4,3 +4,13 @@ const treeA = list(1, 2, 3, 4);
 const treeB = list(list(1, 2), list(3, 4));
 const treeC = null;
 const treeD = list(list(1, 2), null, 3, list(4, null));
+
+// S6 Studio Worksheet
+// Q1
+function my_map(f, xs) {
+    return accumulate((x, ys) => pair(f(x), ys), null, xs);
+}
+
+my_map(math_sqrt, list(1, 2, 3, 4, 5)); 
+
+// Q2
