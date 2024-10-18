c1b38dd403f81827060be8e1ac63d8fd7f0b5d69
Brian Ma <No public email provided>
2024-10-18 16:39:09 +0800

    Changes made from Source Academy

---
+++ 2425studio10.js
@@ -0,0 +1,27 @@
+// // Operator Combinations
+// 2 * 3 + 4 * 5;
+
+
+// // Applications
+// const c = 17; 
+// const e = 2;
+
+// function power(x, e) {
+//     return e === 0
+//           ? 1
+//           : x * power(x, e - 1);
+// }
+// power(c * 4, e + 1);
+
+
+// // Lambda Expressions
+// const f = x => x + 1;
+// f(3);
+
+// Conditionals
+let x = 1;
+if (x > 2) {
+    11;
+} else {
+    22;
+}
