9055d27fdec34b750077adf547d6a46fe8a9accf
Source Academy User <Brian Ma>
2024-10-26 14:35:37 +0800

    Changes made from Source Academy

--- 2425studio11.js
+++ 2425studio11.js
@@ -20,10 +20,29 @@ function ms(m, s) {
     return s;
 }
 
-const ones = pair(1, () => ones);
+// const ones = pair(1, () => ones);
 
-const onesA = pair(1, () => ms("A", onesA));
-stream_ref(onesA, 3);
+// const onesA = pair(1, () => ms("A", onesA));
+// stream_ref(onesA, 3);
 
 // const onesB = pair(1, memo_fun(() => ms("B", onesB)));
 // stream_ref(onesB, 3);
+
+
+// Key thing to note:
+// Everytime we call m_integers_from, we are creating a new function that is "memoized"
+// Therefore, we have a bunch of "memoized functions".
+function m_integers_from(n) {
+    return pair(n, 
+        memo_fun(() => ms("M: " + stringify(n), 
+                          m_integers_from(n + 1))));
+}
+
+const m_integers = m_integers_from(1);
+
+stream_ref(m_integers, 0);
+stream_ref(m_integers, 1);
+stream_ref(m_integers, 2);
+stream_ref(m_integers, 2);
+// stream_ref(m_integers, 5);
+// stream_ref(m_integers, 5);
